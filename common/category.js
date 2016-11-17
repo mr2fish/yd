d2e3e76b92f60c5cf8d45219858d0bee1607c849
lyf <1601942611@qq.com>
2016-11-17 11:05:08 +0800

    提取icon公共目录

--- common/category.js
+++ common/category.js
@@ -3,10 +3,13 @@
  *   定义好的模块，在其他一个页面
  */
 const defaultItem = '不限'
+// 注意：iconPath并不是相对于当前文件的相对目录
+// 而是用到icon字段的页面（filter.wxml）所相对的页面
+const iconPath = './icon'
 const categorys = {
   relation: {
     name: 'relation',
-    icon:'icon/object.png',
+    icon:`${iconPath}/object.png`,
     title: '关系',
     allTitle: '送礼对象',
     items: ["爸爸","妈妈","老公男友","老婆女友","基友","闺蜜","朋友","同事","老板"],
@@ -16,13 +19,13 @@ const categorys = {
     name:'scene',
     title: '场景',
     allTitle: '送礼场景',
-    icon:'icon/scene.png',
+    icon:`${iconPath}/scene.png`,
     items: ["圣诞节","新年","过年回家","情人节","生日","纪念日","告白","乔迁","新婚"],
     selectedIndex: -1
   },
   category: {
     name: 'category',
-    icon:'icon/gifts.png',
+    icon:`${iconPath}/gifts.png`,
     title: '品类',
     allTitle: '送礼品类',
     items: ["家具家装","生活日用","家用电器","办公用品","时尚悦己","个护化妆","科技数码","运动健康","吃喝旅行","母婴玩具","汽车户外"],
@@ -30,7 +33,7 @@ const categorys = {
   },
   price: {
     name: 'price',
-    icon:'icon/price.png',
+    icon:`${iconPath}/price.png`,
     title: '价格',
     allTitle: '礼物价格',
     items: ["0-200","200-500","500-800","800+"],
