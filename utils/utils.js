d67a37b39a9a64826f1a84c02f6aed575d3bc233
lyf <1601942611@qq.com>
2016-11-18 15:55:26 +0800

    更具方法注释

--- utils/utils.js
+++ utils/utils.js
@@ -64,109 +64,6 @@ export function type(arg){
                   Object.prototype.toString.call(arg).slice(8,-1).toLowerCase():t
 }
 
-// 判断一个对象是否是函数
-export function isFunction( fn ){
-      return type( fn ) === 'function';
-};
-
-// 判断是否为普通对象
-// 即简单的字典
-// { id:xx, name:xx } return true
-export function isPlainObject( obj ){
-      let key;
-      // 过滤非对象和global对象
-      // 小程序中可以认为wx就是global
-      if( type( obj ) !== 'object'){
-            return false;
-      }
-      const hasOwn = Object.prototype.hasOwnProperty;
-      // 这个对象不能是自定义构造器new 出来的
-      // 且对象构造器的prototype属性必须有自己的 isPrototypeOf 属性（其实判断7个内置属性都行，不一定非要判断这个）...
-      if( obj.constructor &&
-             !hasOwn.call( obj , 'constructor' ) &&
-             !hasOwn.call( obj.constructor.prototype || {} , 'isPrototypeOf')  ) {
-            return false;
-      }
-      // key in 会先遍历自有属性，如果最后一个属性都是自有属性的话，说明整个
-      // 对象上所有属性都是自有属性，说明这个对象就是一个简单的字典
-      for( key in obj ) {}
-      return key === void 0 || hasOwn.call( obj , key );
-};
-
-// 短id转长id
-export function getLongCid( cid ){
-  if( cid == void 0 ) return void 0;
-  const C = Math.pow(2,32);
-     // 如果cid大于常数，我们认为就是长ID，直接返回即可，否则再进行处理
-  return cid > C ? cid : ( C + 1 ) * cid;
-}
-
-export function copy(obj){
-  if(typeof obj === 'object'){
-    return JSON.parse(JSON.stringify(obj))
-  }
-}
-
-export function handleTitle(title = []){
-  if(Array.isArray(title)){
-    return title.join('')
-  }
-  return ''
-}
-
-const LIKES_KEY = 'likes'
-
-export function uniquePush(arr, ele){
-  let ret = false;
-  if(arr && Array.isArray(arr) && arr.indexOf(ele) === -1){
-       arr.push(ele)
-       ret = true
-  }
-  return ret
-}
-
-export function getLikesFromStorage(){
-  let likes = wx.getStorageSync(LIKES_KEY)
-  // console.log(likes);
-  if(!likes || !Array.isArray(likes)){
-      likes = []
-  }
-  return likes
-}
-
-export function setLikesToStorage(likes, cid){
-  let ls = getLikesFromStorage()
-  uniquePush(ls, cid)
-  console.log(ls);
-  console.log( ls.indexOf(cid) );
-  wx.setStorageSync(LIKES_KEY, ls)
-}
-
-export function removeLikesFromStorate(){
-  return wx.removeStorageSync(LIKES_KEY)
-}
-
-export function extractPriceFromPriceString(priceString){
-  let ret = 0
-  if(priceString){
-    const priceReg = /\d+(\.\d+)?/
-    const match = priceReg.exec(priceString)
-    if(match){
-      ret = match[0]
-    }
-  }
-  return Number(ret)
-}
-
-export function objectToQueryString(dataObject){
-  if(!dataObject || typeof dataObject !== 'object'){
-    return ''
-  }
-  const kvArr = []
-  Object.keys(dataObject).forEach(key => kvArr.push(`${key}=${dataObject[key]}`))
-  return kvArr.join('&')
-}
-
 export function isNullObject(obj){
   return obj == null?
               true :
@@ -208,37 +105,130 @@ export function isArrayLike(obj){
 	return type === "array" || length === 0 || typeof +length === "number" && length > 0 && (length - 1) in obj;
 }
 
+// 是否是数字或数字类型字符串。
+// 123   -> true
+// '123' -> true
 export function isNumeric(obj){
   var str = obj && obj.toString();
   return type(obj)!=="array" && ( str - parseFloat( str ) + 1 ) >= 0;
 }
 
+// 判断一个对象是否是函数
+export function isFunction( fn ){
+      return type( fn ) === 'function';
+};
 
-// export {copy,handleTitle,uniquePush,getLikesFromStorage,setLikesToStorage,removeLikesFromStorate}
-// function a(){
-//   console.log('I am function a')
-// }
-// function b(){
-//   console.log('I am function b')
-// }
-// function c(){
-//   console.log('I am function c and i am a default export 2')
-// }
-// export const PI = Math.PI;
-// export var num = 1
-// export let numberObject = new Number(num)
-// export var str = 'liyanfeng'
-// export let stringObject = new String(str)
-// export let bool = true;
-// export const booleanObject = new Boolean(bool);
-// export var Null = null
-// export let Undefined = void 0
-// export let symbol = Symbol('liyanfeng')
-// export const object = {name: '李彦峰',age: 26}
-// var obj = {0:0,1:1,length:2}
-// export var sayName = function(){
-//   console.log('我是李彦峰')
-// }
-// export const arr = [1,2,3,obj]
-// export { a, b }
-// export default c
+// 判断是否为普通对象
+// 即简单的字典
+// { id:xx, name:xx } -> true
+export function isPlainObject( obj ){
+      let key;
+      // 过滤非对象和global对象
+      // 小程序中可以认为wx就是global
+      if( type( obj ) !== 'object'){
+            return false;
+      }
+      const hasOwn = Object.prototype.hasOwnProperty;
+      // 这个对象不能是自定义构造器new 出来的
+      // 且对象构造器的prototype属性必须有自己的 isPrototypeOf 属性（其实判断7个内置属性都行，不一定非要判断这个）...
+      if( obj.constructor &&
+             !hasOwn.call( obj , 'constructor' ) &&
+             !hasOwn.call( obj.constructor.prototype || {} , 'isPrototypeOf')  ) {
+            return false;
+      }
+      // key in 会先遍历自有属性，如果最后一个属性都是自有属性的话，说明整个
+      // 对象上所有属性都是自有属性，说明这个对象就是一个简单的字典
+      for( key in obj ) {}
+      return key === void 0 || hasOwn.call( obj , key );
+}
+
+// 短id转长id
+export function getLongCid( cid ){
+  if( cid == void 0 ) return void 0;
+  const C = Math.pow(2,32);
+     // 如果cid大于常数，我们认为就是长ID，直接返回即可，否则再进行处理
+  return cid > C ? cid : ( C + 1 ) * cid;
+}
+
+// ArrayLike转Arr
+export function toArray(arr){
+  const ret = []
+  if(!arr) return ret
+  if(!isArrayLike(arr)) return ret
+  return Array.from(arr)
+}
+
+// 简单的深拷贝方法
+export function copy(obj){
+  if(type(obj) === 'object'){
+    return JSON.parse(JSON.stringify(obj))
+  }else if(Array.isArray(obj)){
+    return [...obj]
+  }
+}
+
+// 处理title
+// ['这是标题1', '这是标题2'] -> '这是标题1这是标题2'
+export function handleTitle(title = []){
+  if(Array.isArray(title)){
+    return title.join('')
+  }
+  return ''
+}
+
+// 数组唯一推入方法，如果数组中不含该元素则push之，否则忽略
+export function uniquePush(arr, ele){
+  let ret = false;
+  if(arr && Array.isArray(arr) && arr.indexOf(ele) === -1){
+       arr.push(ele)
+       ret = true
+  }
+  return ret
+}
+
+// 逛一逛页面工具方法与常数 -- start
+const LIKES_KEY = 'likes'
+export function getLikesFromStorage(){
+  let likes = wx.getStorageSync(LIKES_KEY)
+  // console.log(likes);
+  if(!likes || !Array.isArray(likes)){
+      likes = []
+  }
+  return likes
+}
+
+export function setLikesToStorage(likes, cid){
+  let ls = getLikesFromStorage()
+  uniquePush(ls, cid)
+  console.log(ls);
+  console.log( ls.indexOf(cid) );
+  wx.setStorageSync(LIKES_KEY, ls)
+}
+
+export function removeLikesFromStorate(){
+  return wx.removeStorageSync(LIKES_KEY)
+}
+// 逛一逛页面工具方法与常数 -- start
+
+// 根据传入的价格字符串提取价格 '约￥180.23元' -> 180.23
+export function extractPriceFromPriceString(priceString){
+  let ret = 0
+  if(priceString){
+    const priceReg = /\d+(\.\d+)?/
+    const match = priceReg.exec(priceString)
+    if(match){
+      ret = match[0]
+    }
+  }
+  return Number(ret)
+}
+
+// {name:'李彦峰',age:26} -> name=李彦峰&age=26
+export function objectToQueryString(dataObject){
+  if(!dataObject || typeof dataObject !== 'object'){
+    return ''
+  }
+  const kvArr = []
+  Object.keys(dataObject).forEach(key => kvArr.push(`${key}=${dataObject[key]}`))
+  return kvArr.join('&')
+}
